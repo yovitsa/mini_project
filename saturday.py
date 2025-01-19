93ac5e16fdabc0e2d70a75ef4778fadecf9a8b74
yovitsa <jovica04@outlook.com>
2025-01-18 20:08:12 -0800

    excersize 11

--- saturday.py
+++ saturday.py
@@ -1,9 +1,19 @@
-import random
-a = random.sample(range(1,50),5)
-b = random.sample(range(1,50),5)
+# import random
+# a = random.sample(range(1,50),5)
+# b = random.sample(range(1,50),5)
 
-c = [num for num in a if num in b]
+# c = [num for num in a if num in b]
 
-print(a)
-print(b)
-print(c)
+# print(a)
+# print(b)
+# print(c)
+
+def is_prime(number):
+    if number <= 1:
+        return 'Not Prime number'
+    for num in range(2, int(number ** 0.5)+1):
+        if number % 1 == 0:
+            return 'Not a prime'
+    return 'Prime Number'
+    
+print(is_prime(724))
