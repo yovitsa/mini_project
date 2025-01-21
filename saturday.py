7151db683ed6f34489fa29c66d26507f58d6d953
yovitsa <jovica04@outlook.com>
2025-01-20 18:18:47 -0800

    daily post

--- saturday.py
+++ saturday.py
@@ -8,12 +8,22 @@
 # print(b)
 # print(c)
 
-def is_prime(number):
-    if number <= 1:
-        return 'Not Prime number'
-    for num in range(2, int(number ** 0.5)+1):
-        if number % 1 == 0:
-            return 'Not a prime'
-    return 'Prime Number'
+# def is_prime(number):
+#     if number <= 1:
+#         return 'Not Prime number'
+#     for num in range(2, int(number ** 0.5)+1):
+#         if number % 1 == 0:
+#             return 'Not a prime'
+#     return 'Prime Number'
     
-print(is_prime(724))
+# print(is_prime(724))
+
+import random
+
+# b = random.sample(range(1,30), 7)
+
+def first_and_last(a_list):
+    
+    return [a_list[0], a_list[len(a_list) -1]]
+
+print(first_and_last(a_list = random.sample(range(1,30),29)))
